e48428b0cbfa692acea5c6f1f0bb10e6e61ed59d
WilverI <wilb123@umes.edu.gt>
2023-09-07 19:03:34 -0600

    Controlador de clientes

---
+++ controllers/externo/clienteController.js
@@ -0,0 +1,153 @@
+'use strict'
+const Sequelize     = require('sequelize');
+const db = require("../../models");
+const moment = require('moment');
+const axios = require('axios')
+
+module.exports = {
+    async find (req, res) {
+        let id = req.body.id
+        const options = {
+            'method': 'GET',
+            'url': 'http://localhost:5000/cliente/find/id',
+            'headers': {
+              'Content-Type': 'application/json'
+            },
+            data: {
+                id: id
+            }
+        };
+        
+        try {
+            const result = await axios(options);
+            console.log(result.data)
+            if (result.data.id !== undefined) {
+                const resultado = result.data
+                const mensaje = "El cliente con id " + resultado.id + " a nombre de " + resultado.nombre
+                res.status(200).send(mensaje)
+            }
+            res.status(404).send("La cliente no existe en el otro servicio")
+        } catch (e) {
+            console.log(e);
+        } 
+    },
+
+    async create(req, res) {
+        const { nombre, apellido, fecha_nacimiento, telefono, direccion, email, tipo_id } = req.body;
+
+        // Validar que se proporcionen los campos necesarios
+        if (!nombre || !apellido || !fecha_nacimiento || !telefono || !direccion || !email || !tipo_id) {
+            return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+        }
+
+        // Crear un objeto con los datos
+        const datos_ingreso = {
+            nombre, 
+            apellido, 
+            fecha_nacimiento, 
+            telefono, 
+            direccion, 
+            email, 
+            tipo_id
+        };
+
+        // Envía la solicitud para crear el cliente en el otro servicio
+        const createOptions = {
+            method: 'POST',
+            url: 'http://localhost:5000/cliente/create',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: datos_ingreso
+        };
+
+        try {
+            const result = await axios(createOptions);
+            console.log(result.data);
+            // Aquí puedes manejar la respuesta del otro servicio como lo desees
+            if(result.data !== undefined){
+                const resultado = result.data
+                const mensaje = "El cliente con id " + resultado.id + " a nombre de " + resultado.nombre + " ha sido creado"
+                return res.status(201).send(mensaje)
+            }
+            res.status(500).json({ error: 'Error al insertar el cliente en el otro servicio' });
+        } catch (error) {
+            console.log(e);
+        }
+    },
+
+    async update(req, res) {
+        const { id, nombre, apellido, fecha_nacimiento, telefono, direccion, email, tipo_id } = req.body;
+
+        // Validar que se proporcionen los campos necesarios
+        if (!nombre || !apellido || !fecha_nacimiento || !telefono || !direccion || !email || !tipo_id) {
+            return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+        }
+
+        // Crear un objeto con los datos
+        const datos_ingreso = {
+            id,
+            nombre, 
+            apellido, 
+            fecha_nacimiento, 
+            telefono, 
+            direccion, 
+            email, 
+            tipo_id
+        };
+
+        // Envía la solicitud para actualizar el cliente en el otro servicio
+        const updateOptions = {
+            method: 'PUT',
+            url: 'http://localhost:5000/cliente/update',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: datos_ingreso
+        };
+
+        try {
+            const result = await axios(updateOptions);
+            console.log(result.data);
+            // Aquí puedes manejar la respuesta del otro servicio como lo desees
+            if (result.data !== undefined) {
+                const resultado = result.data
+                const mensaje = "El cliente con id " + resultado.id + " a nombre de " + resultado.nombre + " ha sido actualizado"
+                return res.status(201).send(mensaje)
+            }
+            res.status(500).json({ error: 'Error al actualizar el cliente en el otro servicio' });
+        } catch (error) {
+            console.log(error); 
+        }
+    },
+    // async delete(req, res) {
+    //     const id = req.params.id;
+    
+    //     // Validar que se proporcione un ID válido
+    //     if (!id) {
+    //         return res.status(400).json({ error: 'Se requiere un ID válido' });
+    //     }
+    
+    //     // Envía la solicitud para eliminar el tipo en el otro servicio
+    //     const deleteOptions = {
+    //         method: 'DELETE',
+    //         url: `http://localhost:5000/cliente/delete/${id}`,
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     };
+    
+    //     try {
+    //         const result = await axios(deleteOptions);
+    //         console.log(result.data);
+    //         // Aquí puedes manejar la respuesta del otro servicio como lo desees
+    //         if (result.data && result.data.message === 'Tipos eliminado correctamente') {
+    //             return res.status(200).json({ message: 'Tipo eliminado correctamente' });
+    //         }
+    //         res.status(500).json({ error: 'Error al eliminar el tipo en el otro servicio' });
+    //     } catch (error) {
+    //         console.error('Error al eliminar el tipo:', error);
+    //         return res.status(500).json({ error: 'Error al eliminar el tipo' });
+    //     }
+    // }
+}
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = Router();
 // facturas
 const facturasController = require('../controllers/cuenta/facturaControler');
 const externoController = require('../controllers/externo/TipoController');
+const clienteController = require('../controllers/externo/clienteController')
 
 //RUTAS
 
@@ -16,6 +17,12 @@ module.exports = (app) => {
     router.post('/externo/create', externoController.create);
     router.put('/externo/update', externoController.update);
     router.delete('/externo/delete/:id', externoController.delete);
+
+    router.get('/cliente/find', clienteController.find);
+    router.post('/cliente/create', clienteController.create);
+    router.put('/cliente/update', clienteController.update);
+    // router.delete('/cliente/delete/:id', clienteController.delete);
+
 
     app.use('/', router);
 
