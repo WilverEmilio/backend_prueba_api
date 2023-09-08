2d3923309a0d9720d39c2cc6d887e81b65bc5026
WilverI <wilb123@umes.edu.gt>
2023-09-07 18:30:22 -0600

    envio de datos desde la API

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const { Router } = require('express');
+const router = Router();
+
+// facturas
+const facturasController = require('../controllers/cuenta/facturaControler');
+const externoController = require('../controllers/externo/externoController');
+
+//RUTAS
+
+module.exports = (app) => {
+    //facturas
+    router.get('/factura/find', facturasController.find);
+    router.post('/contabilidad/create', facturasController.create);
+
+    router.get('/externo/find', externoController.find);
+    router.post('/externo/create', externoController.create);
+    router.put('/externo/update', externoController.update);
+    router.delete('/externo/delete/:id', externoController.delete);
+
+    app.use('/', router);
+
+};
